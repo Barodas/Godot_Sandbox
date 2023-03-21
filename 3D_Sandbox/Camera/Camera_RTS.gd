f4b58b81ba0b6a811b1c8af1f32364cf5c4c7bbb
Barodas <lsava22@hotmail.com>
2023-03-21 18:15:19 +1000

    Added CameraRoot scene and started RTS style camera controller

---
+++ 3D_Sandbox/Camera/Camera_RTS.gd
@@ -0,0 +1,24 @@
+extends Camera
+
+
+var speed = 3
+
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	var move_x = 0
+	var move_z = 0
+	
+	if Input.is_action_pressed("camera_left"):
+		move_x = -1
+	if Input.is_action_pressed("camera_right"):
+		move_x = 1
+	if Input.is_action_pressed("camera_forward"):
+		move_z = -1
+	if Input.is_action_pressed("camera_back"):
+		move_z = 1
+	
+	transform.origin = transform.origin + Vector3((move_x * speed) * delta, 0, (move_z * speed) * delta)
